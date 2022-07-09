64c0be2d38b5d10d19da26d52f07f482d60c9135
altstronaut <altstronaut@gmail.com>
2022-07-09 18:00:01 -0500

    chore: adding HappyNumber

---
+++ src/main/kotlin/com.altstronaut.hashtable/HappyNumber.kt
@@ -0,0 +1,26 @@
+package com.altstronaut.hashtable
+
+import com.altstronaut.hastable.missingNumber
+
+fun isHappy(n: Int): Boolean {
+    var myNum = n
+    val set = mutableSetOf<Int>()
+    while (!set.contains(myNum) && myNum != 1) {
+        set.add(myNum)
+        val digits = myNum.toDigits()
+        var sum = 0
+        digits.forEach {
+            sum = sum + (it * it)
+        }
+        myNum = sum
+    }
+    return myNum == 1
+}
+
+fun Int.toDigits(): List<Int> = toString().map { it.toString().toInt() }
+
+fun main(args: Array<String>) {
+    val num = 19
+    val result = isHappy(num)
+    println(result)
+}
